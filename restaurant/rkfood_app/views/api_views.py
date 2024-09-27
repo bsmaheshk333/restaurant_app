b8862fcc68e7f8bd96537f6c11a9b89c71e2d831
bsmaheshk333 <bsmaheshk333@gmail.com>
2024-09-27 21:17:19 +0530

    implemented rest apis and modified views.py

--- restaurant/rkfood_app/views/api_views.py
+++ restaurant/rkfood_app/views/api_views.py
@@ -1,5 +1,91 @@
-from rest_framework.views import APIView, Response
+from rest_framework.views import APIView, Response, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rkfood_app.serializers import (LoginUserSerializer,
+                                    RegisterUserSerializer,
+                                    CustomerSerializer)
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from rkfood_app.models import Customer
 
 
+class LoginApiView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response({'error': f"you're not authenticated to access this page"},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        if not request.user.is_active:
+            return Response({"detail": "No active account found with the given credentials"},
+                            status=status.HTTP_403_FORBIDDEN)
+        return Response({'error': 'method not allowed'},
+                        status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def post(self, request):
+        username: str = request.data.get('username', None).strip()
+        password: str = request.data.get('password', None).strip()
+        print(f"{username = } and {password = }")
+        if not username or not password:
+            return Response({'msg': 'invalid username or password'}, status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        print(f"{user = }")
+        if user:
+            return Response({"msg": f'login success {user.id}'}, status=status.HTTP_200_OK)
+        return Response({'msg': 'login failed due to bad request'},status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class RegisterApiView(APIView):
+    def get(self, request):
+        # added permission token here.
+        # role based authentication
+        if request.user.is_staff:
+            # serializers = RegisterUserSerializer(instance=User)
+            # only staff is given access to see the user details
+            users = User.objects.all().values('username', 'email', 'password')
+            return Response({'user': list(users)}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'permission denied'}, status=status.HTTP_403_FORBIDDEN)
+        # return Response({'error': 'method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def post(self, request):
+        if not request.user.is_staff:
+            return Response({'auth_err': 'access restricted'}, status.HTTP_403_FORBIDDEN)
+        username: str = request.data.get('username', None)
+        password: str = request.data.get('password', None)
+        email = request.data.get('email', None)
+        phone: str = request.data.get('phone', None)
+        errors = {}
+        # validate username
+        if username in ['admin', 'root', 'superuser']:
+            errors['username'] = f'username {username} is restricted'
+        is_username_exist = User.objects.filter(username=username).exists()
+        if is_username_exist:
+            errors['username_exist'] = (f'this username {username} exist already Please try with '
+                                        f'different name.')
+        # validate password
+        if len(password) < 8 or not any(char.isdigit() for char in password):
+            errors['password'] = ("password criteria not met.\n"
+                                  "1. password must contain at least one digit/numeric"
+                                  "2. password must be 8 characters long")
+        # validate email
+        try:
+            validate_email(email)
+        except ValidationError:
+            errors['email'] = "ERROR! invalid email address."
+
+        if len(phone) != 10:
+            errors['phone'] = "please check if phone has exactly 10 digits and contains only numbers"
+        elif Customer.objects.filter(phone=phone).exists():
+            errors['phone'] = "this number exist already."
+        if errors:
+            return Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            Customer.objects.create(user=user, phone=phone)
+        except:
+            pass
+        return Response({'register': 'user registered successfully'}, status=status.HTTP_201_CREATED)
--- restaurant/rkfood_app/views/fb_views.py
+++ restaurant/rkfood_app/views/fb_views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import (render, redirect,
-                              get_object_or_404,
+                              get_object_or_404, HttpResponseRedirect,
                               Http404)
 from rkfood_app.models import (Restaurant,Menu,
                                MenuItems,
                                Customer,
                                Order)
+from django.urls import reverse
+from django.db.models import Q
 from django.http import JsonResponse
 from django.template import TemplateDoesNotExist
 from django.contrib.auth import login, logout, authenticate
@@ -16,76 +18,97 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 import string
-
+from django.db import IntegrityError
 
 
 base_template = 'base.html'
 
 
+@login_required(login_url="login/")
 def home(request):
     context = {}
     try:
-        menu_items_qs = MenuItems.objects.all()
+        menu_items = MenuItems.objects.all()
+        for item in menu_items:
+            print("menu title =", item.menu.menu_title)
+        breakfast = Menu.objects.filter(menu_title="Breakfast")
         restaurant = Restaurant.objects.all().first()
         if restaurant:
             close_time = restaurant.formatted_close_time
-            print(f"type {type(close_time)}")
         else:
             close_time = None
-        print(f"{close_time = } ")
-        context['menu_items'] = menu_items_qs
-        context['close_time'] = close_time
+        # context['menu_items'] = menu_items_qs
+        # context['close_time'] = close_time
+        context = {
+            'menu_items': menu_items,
+            'close_time': close_time,
+            'breakfast': breakfast
+        }
         return render(request, base_template, context)
     except MenuItems.DoesNotExist:
         return JsonResponse({"error": 'Model data not exist.'}, status=400)
     except TemplateDoesNotExist as ex:
         return JsonResponse({'error': 'template not exist'}, status=400)
-        # return HttpResponse(f"""<h1 class="m" style='text-align:center; color: red;'>
-        # Templates does not exist</h1>""")
 
 
-def search_item(request):
-    # TODO redis
-    context = {}
-    if request.method == 'POST':
-        query_item = request.POST.get('query_item', None)
-        # you can have multiple filter depending on requirement
-        # if request.method == 'POST':
-        if query_item:
-            try:
-                model_qs = MenuItems.objects.filter(name__icontains=query_item)
-                context['menu_items'] = model_qs
-                print(f"{query_item = }")
-                return render(request, 'search_items.html', context)
-            except TemplateDoesNotExist:
-                return JsonResponse({'error': 'template does not exist'}, status=400)
-        else:
-            return JsonResponse({'error': 'query item not provided'}, status=400)
-    else:
-        return JsonResponse({'error': 'method not allowed'}, status=400)
+def item_detail_view(request, slug):
+    item = MenuItems.objects.get()
 
-def customer_profile(request, user_id):
+def customer_profile(request, id):
     # TODO implement redis
-    profile_data = get_object_or_404(Customer, pk=user_id)
-    context = {'profile_data': profile_data}
-    return render(request, 'customer/profile.html')
+    try:
+        context = {}
+        user = User.objects.get(id=id)
+        profile = Customer.objects.get(user=user)
+        # if request.user.is_staff or request.user.is_superuser:
+        #     return HttpResponseRedirect(reverse("admin:index"))
+        context = {'profile': profile}
+        return render(request, 'customer/profile.html',context)
+    except Customer.DoesNotExist:
+        # do not use JsonResponse render html page instead
+        return JsonResponse({'Message': 'user is admin, Please visit the admin site.'}, status=404)
+    except TemplateDoesNotExist:
+        # do not use JsonResponse render html page instead
+        return JsonResponse({'error': 'Template does not exist'}, status=404)
+
 
 def customer_login(request):
+    errors = {}
+    # if user is disabled or removed
+    if request.user.is_authenticated:
+        errors['error'] = 'you must be logged in to access this page'
     if request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
-        username = User.objects.get(username=request.user.username)
+        # Validate username and password presence
+        if not username:
+            errors['username'] = "Invalid username."
+        if not password:
+            errors['password'] = "Invalid password."
 
-        user = authenticate(username=username, password=password)
-        if user is None:
-            return JsonResponse({'error': f'invalid username or password'},
-                                status=400)
-        else:
-            print('login success')
-            login(request, user)
-            return redirect('/')
-    else:
-        return render(request, "customer/login.html")
+        if not errors:
+            # if username and password are provided only then you authenticate
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                with open("user_logged_data.txt", mode="a") as log_file:
+                    print(f"{log_file = }")
+                    log_file.write(f"user: {request.user}")
+                return redirect('/')
+            else:
+                errors['invalid_creds'] = 'Invalid credentials.'
+        # FIXME do not write this in production, json is meant for API
+        return JsonResponse({
+            'errors': errors,
+            'username': username,
+            'password': password,
+        })
+        # return render(request, "customer/login.html",
+        #               {'errors': errors,'username': username,'password': password,})
+
+    # If GET request, just render the login form
+    return render(request, "customer/login.html")
+
 
 def customer_register(request):
     if request.method == 'POST':
@@ -104,31 +127,67 @@ def customer_register(request):
         if is_username_exist:
             errors['username'] = 'user with this name exist'
 
+        if username in ['admin', 'root', 'superuser']:
+            errors['username'] = f'the username {username} is restricted.'
+
         if not any(char.isdigit() for char in password):
             errors['password'] = 'password must contain at least 1 digit.'
 
         if not phone.isdigit() or len(phone) < 10:
             errors['phone'] = "phone number must be digits and must be at least 10 digits."
 
+        is_phone_no_exist = Customer.objects.filter(phone=phone).exists()
+        if is_phone_no_exist:
+            errors['phone'] = "Phone number exist already"
+
         if errors:
+            # better to consider render html template to showcase the error
             return JsonResponse(errors, status=400)
-        user, created = User.objects.get_or_create(username=username,password=password, email=email)
-                                                   #default= {'password': password, 'email':email})
-        if created:
-            Customer.objects.create(user=user, phone=phone)
-        else:
-            # update phone number if user exist already
-            user.customer_profile.phone = phone
-            user.customer_profile.save()
-        return redirect("login")
+        try:
+            user, created = User.objects.get_or_create(username=username, email=email)
+                                                       #default= {'password': password, 'email':email})
+            if created:
+                user.set_password(password)
+                user.save()
+                Customer.objects.create(user=user, phone=phone)
+            else:
+                # update phone number if user exist already
+                user.customer_profile.phone = phone
+                user.customer_profile.save()
+            return redirect("login")
+        except:
+            errors['db'] = 'database error occurred! Please try again.'
+            return JsonResponse(errors, status=400)
     else:
         return render(request, "customer/register.html")
 
 
 def customer_logout(request):
     logout(request)
-    return redirect("/")
+    with open("user_logged_data.txt", mode="a") as log_file:
+        print(f"{log_file = }")
+    return redirect("base")
 
+
+def search_menu_item(request):
+    # TODO redis
+    context = {}
+    if request.method == 'POST':
+        query_item = request.POST.get('query_item', None)
+        # you can have multiple filter depending on requirement
+        # if request.method == 'POST':
+        if query_item:
+            try:
+                model_qs = MenuItems.objects.filter(Q(name__icontains=query_item))
+                context['menu_items'] = model_qs
+                print(f"{query_item = }")
+                return render(request, 'search_items.html', context)
+            except TemplateDoesNotExist:
+                return JsonResponse({'error': 'template does not exist'}, status=400)
+        else:
+            return JsonResponse({'error': 'query item not provided'}, status=400)
+    else:
+        return JsonResponse({'error': 'method not allowed'}, status=400)
 
 def customer_cart(request):
     """
